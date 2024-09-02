9987795b56e7f02ce3965bf972a20aa59337032a
Zhihao/Albert Wang <101526713+xiuhu17@users.noreply.github.com>
2024-09-02 01:20:18 -0500

    update

--- kernel/vm.c
+++ kernel/vm.c
@@ -235,12 +235,14 @@ uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm)
   for(a = oldsz; a < newsz; a += PGSIZE){
     mem = kalloc();
     if(mem == 0){
+      // undo all the mapping so far: uvmdealloc() -> uvmunmap() {*pte = 0; kfree()}
       uvmdealloc(pagetable, a, oldsz);
       return 0;
     }
     memset(mem, 0, PGSIZE);
     if(mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_R|PTE_U|xperm) != 0){
       kfree(mem);
+      // undo all the mapping so far: uvmdealloc() -> uvmunmap() {*pte = 0; kfree()}
       uvmdealloc(pagetable, a, oldsz);
       return 0;
     }
